fd779bdfc63ccec69010ab1e85e638edb46aede9
xc <102371032+tloxxc@users.noreply.github.com>
2022-07-25 10:13:20 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "clan://localhost/AcatTV/box/qj.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
